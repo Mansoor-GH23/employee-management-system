d484722480e5534f558c33cab55c80db58adc980
Mansoor-GH23 <mansoor.ul.hasan@hotmail.com>
2025-07-09 13:03:40 -0700

    Add project files.

---
+++ EmployeeManagementSystem.Api/Repositories/IEmployeeRepository.cs
@@ -0,0 +1,13 @@
+﻿using EmployeeManagementSystem.Api.Models;
+
+namespace EmployeeManagementSystem.Api.Repositories
+{
+    public interface IEmployeeRepository
+    {
+        Task<IEnumerable<Employee>> GetAllAsync();
+        Task<Employee?> GetByIdAsync(int id);
+        Task<Employee> AddAsync(Employee employee);
+        Task UpdateAsync(Employee employee);
+        Task DeleteAsync(Employee employee);
+    }
+}
