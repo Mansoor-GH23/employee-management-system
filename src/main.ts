44275d2ae75eccd7a7789ce0fdda9fb264777e6e
Mansoor-GH23 <123630060+Mansoor-GH23@users.noreply.github.com>
2025-07-10 08:43:26 -0700

    Delete src directory

--- src/main.ts
+++
@@ -1,7 +0,0 @@
-import 'zone.js'; // 👈 Add this line at the very top
-import { bootstrapApplication } from '@angular/platform-browser';
-import { AppComponent } from './app/app';
-import { appConfig } from './app/app.config';
-
-bootstrapApplication(AppComponent, appConfig)
-.catch(err => console.error(err));
