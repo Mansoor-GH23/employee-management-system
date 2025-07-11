67a375b4c2a1f7c5e10a00a6429c3fd91201ab7b
Mansoor-GH23 <mansoor.ul.hasan@hotmail.com>
2025-07-11 11:53:05 -0700

    feat(department-api):add department controller to backend

---
+++ Backend/EmployeeManagementSystem.Api/Controllers/DepartmentsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeManagementSystem.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class DepartmentsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetDepartments()
+        {
+            var departments = new[] { "HR", "IT", "Finance" };
+            return Ok(departments);
+        }
+    }
+}
