028d1b9352fbddccd0281f458702daf0439cdf41
Mansoor-GH23 <mansoor.ul.hasan@hotmail.com>
2025-08-11 12:49:42 -0700

    New Push

--- frontend/src/environments/environment-prod.ts
+++ frontend/src/environments/environment-prod.ts
@@ -1,4 +1,4 @@
-// frontend/src/environments/environment.prod.ts
 export const environment = {
-  production: true
+  production: true,
+  apiBaseUrl: 'https://ems-api-app-gje0ckgkcnhfcmbd.eastus2-01.azurewebsites.net/swagger/index.html'
 };
